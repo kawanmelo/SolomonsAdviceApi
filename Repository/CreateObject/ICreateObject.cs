a3c9a945a1be8a9553521c648f046133e00dd530
kawanmelo <kawangcmelo@gmail.com>
2024-01-07 20:40:16 -0300

    Deleting unnecessary files

--- Repository/CreateObject/ICreateObject.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Data.SqlClient;
-using SolomonsAdviceApi.SolomonAdviceClass;
-
-namespace SolomonsAdviceApi.Repository.CreateObject{
-    public interface ICreateObject{
-        public SolomonAdvice Create(int Id, string Advice, string Reference);
-    }
-}
--- Repository/CreateUpdateDeleteVerses/ICud.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace MeloSolution.Repository.CUD{
-    public interface ICud{
-        public bool CudObject(string sqlQuery, object parameters = null);
-    }
-}
